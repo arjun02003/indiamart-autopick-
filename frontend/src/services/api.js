172473b55a9e9f41836d5c1b53bfb5c9eaea7bac
arjun02003 <arjunchowhan94@gmail.com>
2026-05-16 21:55:10 -0700

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,46 +1,60 @@
-require("dotenv").config();
+import axios from "axios";
 
-const express = require("express");
-const axios = require("axios");
-const cors = require("cors");
-
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-app.use(cors());
-app.use(express.json());
-
-// Home Route
-app.get("/", (req, res) => {
-  res.send("IndiaMART API Server Running...");
-});
+const API = "http://localhost:5000";
 
 // Get Leads
-app.get("/leads", async (req, res) => {
-  try {
-    const response = await axios.get(
-      "https://mapi.indiamart.com/wservce/buyerLead/",
-      {
-        params: {
-          glusr_crm_key: process.env.INDIAMART_API_KEY,
-        },
-      }
-    );
+export const getLeads = async () => {
+  const res = await axios.get(`${API}/leads`);
+  return res.data;
+};
 
-    res.json({
-      success: true,
-      data: response.data,
-    });
-  } catch (error) {
-    console.log("Lead Fetch Error:", error.message);
+// Accept Lead
+export const acceptLead = async (id) => {
+  const res = await axios.post(`${API}/accept`, { id });
+  return res.data;
+};
 
-    res.status(500).json({
-      success: false,
-      error: error.message,
-    });
-  }
-});
+// Skip Lead
+export const skipLead = async (id) => {
+  const res = await axios.post(`${API}/skip`, { id });
+  return res.data;
+};
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+// Export Leads
+export const exportLeads = async () => {
+  const res = await axios.get(`${API}/export`);
+  return res.data;
+};
+
+// Get Config
+export const getConfig = async () => {
+  const res = await axios.get(`${API}/config`);
+  return res.data;
+};
+
+// Save Config
+export const saveConfig = async (data) => {
+  const res = await axios.post(`${API}/config`, data);
+  return res.data;
+};
+
+// Upload Cookies
+export const uploadCookies = async (cookies) => {
+  const res = await axios.post(`${API}/upload-cookies`, {
+    cookies,
+  });
+
+  return res.data;
+};
+
+// Telegram Test
+export const testTelegram = async (data) => {
+  const res = await axios.post(`${API}/test-telegram`, data);
+  return res.data;
+};
+
+// Clear Leads
+export const clearLeads = async () => {
+  const res = await axios.delete(`${API}/clear`);
+  return res.data;
+};
